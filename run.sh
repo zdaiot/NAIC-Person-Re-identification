f290c8627b4f6edaca97f6db2af2199d9719fd08
zdaiot <zdaiot@163.com>
2019-11-18 01:04:47 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
+model=resnet50
+
 python train.py --optimizer_name=Adam --base_lr=1e-4 --scheduler_name=COS --cos_max=20
 time=$(date "+%m-%d %H:%M:%S")
-mv checkpoints/MGN checkpoints/MGN"${time}"
+mv checkpoints/${model} checkpoints/${model}_"${time}"
 
 python train.py --optimizer_name=Adam --base_lr=1e-4 --scheduler_name=StepLR --step=20
 time=$(date "+%m-%d %H:%M:%S")
-mv checkpoints/MGN checkpoints/MGN"${time}"
+mv checkpoints/${model} checkpoints/${model}_"${time}"
 
 python train.py --optimizer_name=SGD --base_lr=5e-2 --scheduler_name=COS --cos_max=20
 time=$(date "+%m-%d %H:%M:%S")
-mv checkpoints/MGN checkpoints/MGN"${time}"
+mv checkpoints/${model} checkpoints/${model}_"${time}"
 
 python train.py --optimizer_name=SGD --base_lr=5e-2 --scheduler_name=StepLR --step=20
 time=$(date "+%m-%d %H:%M:%S")
-mv checkpoints/MGN checkpoints/MGN"${time}"
+mv checkpoints/${model} checkpoints/${model}_"${time}"
